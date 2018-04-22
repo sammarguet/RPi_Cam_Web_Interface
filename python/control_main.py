a6341a179f3db802f23117c09ef231feb6d5cedf
Samuel Marguet <samuel.marguet@outlook.com>
2018-04-22 12:32:06 +0200

    add start/stop light in conf

--- python/control_main.py
+++ python/control_main.py
@@ -2,26 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 # Define Pinout
-PIN_MOTOR_0     = 21
-PIN_MOTOR_1     = 20
-PIN_LIGHT       = 16
+PIN_MOTOR_0	= 21
+PIN_MOTOR_1	= 20
+PIN_LIGHT	= 16
 PIN_MOTOR_3     = 12
 
-PIN_BUTTON_0    = 25
-PIN_BUTTON_1    = 24
+PIN_BUTTON_0	= 25
+PIN_BUTTON_1	= 24
 
-PIN_OPEN        = 8
-PIN_CLOSED      = 7
+PIN_OPEN	= 8
+PIN_CLOSED	= 7
 
 
 # Init variable
-pin_b0          = 0
-pin_b1          = 0
+pin_b0		= 0
+pin_b1		= 0
 
-pin_open        = 0
-pin_closed      = 0
+pin_open	= 0
+pin_closed	= 0
 
-i               = 0
+i		= 0
 
 # Init GPIO Inpout/Output
 GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
@@ -40,43 +40,42 @@ GPIO.output(PIN_LIGHT, GPIO.HIGH)
 
 
 while 1 :
-        print('i = %d \n' % i)
-        pin_b0          = GPIO.input(PIN_BUTTON_0)
-        pin_b1          = GPIO.input(PIN_BUTTON_1)
-        pin_open        = GPIO.input(PIN_CLOSED)
-        pin_closed      = GPIO.input(PIN_OPEN)
+	print('i = %d \n' % i)
+	pin_b0		= GPIO.input(PIN_BUTTON_0)
+        pin_b1		= GPIO.input(PIN_BUTTON_1)
+        pin_open	= GPIO.input(PIN_CLOSED)
+        pin_closed	= GPIO.input(PIN_OPEN)
 
-        if pin_b0 != 1:
-                print('Fermeture de porte')
-                while pin_closed and pin_b1 :
-                        pin_b1 = GPIO.input(PIN_BUTTON_1)
-                        pin_closed = GPIO.input(PIN_OPEN)
-                        GPIO.output(PIN_MOTOR_0, GPIO.LOW)
+	if pin_b0 != 1:
+		print('Fermeture de porte')
+		while pin_closed and pin_b1 :
+			pin_b1 = GPIO.input(PIN_BUTTON_1)
+		        pin_closed = GPIO.input(PIN_OPEN)
+			GPIO.output(PIN_MOTOR_0, GPIO.LOW)
                         GPIO.output(PIN_MOTOR_1, GPIO.HIGH)
-                GPIO.output(PIN_MOTOR_0, GPIO.LOW)
+		GPIO.output(PIN_MOTOR_0, GPIO.LOW)
                 GPIO.output(PIN_MOTOR_1, GPIO.LOW)
-                if pin_closed !=1:
-                        print('Porte fermee')
-                time.sleep(1)
+		if pin_closed !=1:
+			print('Porte fermee')
+		time.sleep(1)
         if pin_b1 != 1:
-                print('Ouverture de porte')
+		print('Ouverture de porte')
                 while pin_open and pin_b0 :
                         pin_b0 = GPIO.input(PIN_BUTTON_0)
-                        pin_open = GPIO.input(PIN_CLOSED)
+		        pin_open = GPIO.input(PIN_CLOSED)
                         GPIO.output(PIN_MOTOR_0, GPIO.HIGH)
                         GPIO.output(PIN_MOTOR_1, GPIO.LOW)
                 GPIO.output(PIN_MOTOR_0, GPIO.LOW)
                 GPIO.output(PIN_MOTOR_1, GPIO.LOW)
-                if(pin_open!=1):
-                        print('Porte ouverte')
-                time.sleep(1)
-        GPIO.output(PIN_MOTOR_0, GPIO.LOW)
+		if(pin_open!=1):
+			print('Porte ouverte')
+		time.sleep(1)
+	GPIO.output(PIN_MOTOR_0, GPIO.LOW)
         GPIO.output(PIN_MOTOR_1, GPIO.LOW)
 
 
-        time.sleep(0.1)
-        i+=1
+	time.sleep(0.1)
+	i+=1
 
  
 GPIO.output(PIN_LIGHT, GPIO.LOW)
-
